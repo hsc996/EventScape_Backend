30f3bb9e986a6d0ea8a3473b3da561e2617e4dfc
hsc996 <han.scaife@gmail.com>
2024-12-06 23:44:04 +1100

    search controllers added, not tested yet

---
+++ src/controllers/EventSearchControllers.js
@@ -0,0 +1,56 @@
+const express = require("express");
+
+const { validateUserAuth } = require("../middleware/validateUserAuth.js");
+const { handleRoute, sendSuccessResponse } = require("../middleware/routerMiddleware.js");
+const { AppError, handleRouteError } = require("../functions/helperFunctions.js");
+const { findPublicEvents, findPrivateEvents } = require("../utils/crud/EventSearchCrud.js");
+
+const router = express.Router();
+
+router.get("/public",
+    validateUserAuth,
+    handleRoute(async (request, response) => {
+        try {
+            const { query } = request.query;
+
+            if (!query){
+                throw new AppError("Search query is required.", 400);
+            }
+
+            const result = await findPublicEvents(query);
+            if (!result.length){
+                throw new AppError("No public events found matching your search.");
+            }
+
+            sendSuccessResponse(response, "Event search completed successfully.", result);
+        } catch (error) {
+            handleRouteError(response, error, "Could not complete search at this time, please try again later.")
+        }
+    })
+);
+
+
+router.get("/private",
+    validateUserAuth,
+    handleRoute(async (request, response) => {
+        try {
+            const { query } = request.query;
+
+            if (!query){
+                throw new AppError("Search query is required.", 400);
+            }
+
+            const result = await findPrivateEvents(query);
+            if (!result.length){
+                throw new AppError("No private events found matching your search.");
+            }
+
+            sendSuccessResponse(response, "Event search completed successfully.", result);
+        } catch (error) {
+            handleRouteError(response, error, "Could not complete search at this time, please try again later.")
+        }
+    })
+);
+
+
+    module.exports = router;
