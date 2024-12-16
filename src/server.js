8fb7af7d87aeacf2454dde58004a2a10b99de037
hsc996 <han.scaife@gmail.com>
2024-12-16 15:35:21 +1100

    added 5174 to CORS policy

--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ let corsOptions = {
         const validOrigins = [
             "http://localhost:3000",
             "http://localhost:5173",
+            "http://localhost:5174/",
             "http://127.0.0.1:5173",
             "https://eventscape1.netlify.app"
         ];
